0bcac450ca83d17a79ebc22ea6d2eee94ee0e176
leeeeeeeeeeeehr <jysg2908123@gmail.com>
2024-05-31 17:54:55 +0900

    Practice 'pull and push'

--- src/ex07string/Test.java
+++ src/ex07string/Test.java
@@ -3,7 +3,9 @@ package ex07string;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("1. 학원에서 푸시");
+		System.out.println("2. 집에서 복습");
 
 	}
 
